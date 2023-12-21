0ae928486a9884ec22734b8fd0b0410ccb6aa857
vasanth1066 <vasanthanuvavi@gmail.com>
2023-12-21 13:04:41 +0530

    First code in react

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+        <div> 
+            <h3>Expense Item</h3>
+            <div>Food Rs 10</div>
+            <div>Petrol Rs 100</div>
+            <div>Movies Rs 200</div>
+        </div>
+    )
+}
+ export default ExpenseItem;
