a85b0f33edd80e41e3aa2b2fa7bb89378a6bab0b
vasanth1066 <vasanthanuvavi@gmail.com>
2023-12-22 20:18:36 +0530

    Manipulating the DOM - React States - UseState  Hook

--- src/Components/Expenses/ExpenseDetails.js
+++ src/Components/Expenses/ExpenseDetails.js
@@ -1,17 +1,29 @@
+import React, {useState }from 'react';
 import './ExpenseItem.css'
 
 function ExpenseDetails(props){
+
+    const [title,setTitle]=useState(props.title);
+
+    function ChangeTitle(){
+        setTitle('update');
+        // console.log(title) // console.log(' button clicked') // console.log(setTitle)
+    }
+
+    let [amount,setAmount]=useState(props.amount);
+    function ChangeExpense(){
+       setAmount(100);
+    }
+
     function DeleteExpense(){
         console.log('deleteexpense button clicked')
     }
-    function ChangeTitle(){
-        console.log(' button clicked')
-    }
     return(
         <div className="expense-item__description ">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <h2>{props.location}</h2>
-        <div className="expense-item__price ">Rs{props.amount}</div>
+        <div className="expense-item__price ">Rs{amount}</div>
+        <button onClick={ChangeExpense}>Change expense</button>
         <button onClick={ChangeTitle}>Change title</button>
         <button onClick={DeleteExpense}>Delete Expense</button>
     </div>
