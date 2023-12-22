be551df510328fad073d01df5b083f9cdcd64d61
vasanth1066 <vasanthanuvavi@gmail.com>
2023-12-22 18:14:46 +0530

    Adding Event listener onClick

--- src/Components/Expenses/ExpenseDetails.js
+++ src/Components/Expenses/ExpenseDetails.js
@@ -1,11 +1,19 @@
 import './ExpenseItem.css'
 
 function ExpenseDetails(props){
+    function DeleteExpense(){
+        console.log('deleteexpense button clicked')
+    }
+    function ChangeTitle(){
+        console.log(' button clicked')
+    }
     return(
         <div className="expense-item__description ">
         <h2>{props.title}</h2>
         <h2>{props.location}</h2>
         <div className="expense-item__price ">Rs{props.amount}</div>
+        <button onClick={ChangeTitle}>Change title</button>
+        <button onClick={DeleteExpense}>Delete Expense</button>
     </div>
     );
 }
